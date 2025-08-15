deb51aedbe4aad6ceaf170e54adb329f53feab91
NathanielC <deadhotbody@gmail.com>
2025-08-15 15:53:10 +0800

    Finalized Final project code - starter code to be added later

--- script.js
+++ script.js
@@ -1,21 +1,99 @@
-const destinations = [
-    ["London",
-    "London, the vibrant capital of England, is a bustling metropolis known for its rich history and diverse culture. Spanning the River Thames, the city seamlessly blends the old with the new, showcasing iconic landmarks like the Tower of London, Buckingham Palace, and the modern Shard skyscraper. A hub for arts, fashion, and finance, London boasts an array of world-class museums, theaters, and restaurants. Its multicultural neighborhoods, from the historic streets of Covent Garden to the upscale charm of Notting Hill,reflect a dynamic tapestry of global influences, making London a captivating destination for visitors and residents alike.",
+const place = [
+    "London",
+    "Hong Kong",
+    "Singapore",
+    "Tokyo"
+];
+
+const description = [
+    "London, the vibrant capital of England, is a bustling metropolis known for its rich history and diverse culture.",
+    "Hong Kong, a bustling metropolis on the southern coast of China, is known for its striking skyline and vibrant urban life.",
+    "Singapore, a vibrant city-state in Southeast Asia, is renowned for its stunning skyline and multicultural atmosphere.",
+    "Tokyo, the bustling capital of Japan, is a vibrant metropolis that seamlessly blends tradition and innovation."
+];
+
+const icons = [
     "London.png",
-    "London_bg.png"],
-    ["Hong Kong",
-    "Hong Kong, a bustling metropolis on the southern coast of China, is known for its striking skyline and vibrant urban life. Blending Eastern and Western influences, the city offers a unique cultural experience, characterized by its rich history, diverse cuisine, and dynamic arts scene. The iconic Victoria Harbour, flanked by towering skyscrapers, serves as a backdrop for the city's famous light shows. From the bustling markets of Mong Kok to the serene trails of Victoria Peak, Hong Kong is a city of contrasts, where tradition meets modernity, making it a fascinating destination for travelers.",
     "Hong_Kong.png",
-    "Hong_kong_bg.png"
-    ],
-    ["Singapore",
-    "Singapore, a vibrant city-state in Southeast Asia, is renowned for its stunning skyline and multicultural atmosphere. This bustling metropolis combines modern architecture with lush green spaces, exemplified by landmarks like the Marina Bay Sands and the Gardens by the Bay. Known as a global financial hub, Singapore offers a rich tapestry of cultures, reflected in its diverse neighborhoods such as Chinatown, Little India, and Kampong Glam. The city is also famous for its culinary scene, featuring a mix of street food and high-end dining. With its clean streets, efficient public transport, and commitment to sustainability, Singapore is a captivating destination that showcases the best of urban living.",
     "Singapore.png",
-    "Singapore_bg.png"
-    ],
-    ["Tokyo",
-    "Tokyo, the bustling capital of Japan, is a vibrant metropolis that seamlessly blends tradition and innovation. Known for its towering skyscrapers, neon-lit streets, and historic temples, the city offers a unique cultural experience. From the tranquil gardens of the Imperial Palace to the bustling shops of Shibuya and the culinary delights of Tsukiji Market, Tokyo is a feast for the senses. Its efficient public transport system makes it easy to explore diverse neighborhoods, each with its own distinct character. With a rich history, cutting-edge technology, and a dynamic arts scene, Tokyo captivates visitors with its endless energy and charm.",
-    "Tokyo.png",
+    "Tokyo.png"
+];
+
+const place_image = [
+    "London_bg.png",
+    "Hong_kong_bg.png",
+    "Singapore_bg.png",
     "Tokyo_bg.png"
-    ]]
-    
+];
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 4);
+}
+
+let destNum = 1
+
+const introCard = document.getElementById("intro-card")
+const destCard = document.getElementById("destination-card")
+const loadingCard = document.getElementById("loading-card")
+const returnButton = document.getElementById("return-button")
+
+introCard.style.opacity = "1";
+destCard.style.opacity = "0";
+loadingCard.style.opacity = "0";
+
+const cards = [introCard, destCard,loadingCard]
+
+for (let i = 0; i < 3; i++) {
+    cards[i].style.transform = "opacity 0.5s ease-in-out"
+}
+
+function toggleFadeInOut(card1,card2) {
+    card1.style.opacity = '0'; // Start fade out
+    setTimeout(() => {
+        card1.style.display = 'none'; // Hide current div
+        card2.style.display = 'flex'; // Show next div
+        card2.style.opacity = '1'; // Fade in next div
+    }, 500); // Match timeout with CSS transition duration
+}
+
+function loadingCardTransition() {
+    toggleFadeInOut(introCard,loadingCard)
+    let dots = ''
+    let count = 0
+    const interval = setInterval(() => {
+            if (count < 6) { // 6 dots in total for 3 seconds
+                dots += '.';
+                loadingCard.innerHTML = `<h1>Loading${dots}<h1>`
+                count++;
+            } else {
+                dots = ''
+                count = 0
+            }
+    }, 250)
+    setTimeout(() => {toggleFadeInOut(loadingCard, destCard)
+    }, 3000);
+}
+
+introCard.addEventListener('click',() => {
+    destNum = getRandomNumber()
+    // Added Day 3
+    destCard.innerHTML = `
+        <div id="destination-header">
+            <h1>You got: ${place[destNum]}</h1>
+            <img id="header_img" src="assets/${icons[destNum]}">
+        </div>
+        <div id = "main-pic">
+            <img id = "pic" src = "assets/${place_image[destNum]}">
+        </div>
+        <p id="description-text">
+            ${description[destNum]}
+        </p>
+    `
+    loadingCardTransition()
+})
+
+returnButton.addEventListener('click', () => {
+    toggleFadeInOut(destCard,introCard)
+})
+
+
